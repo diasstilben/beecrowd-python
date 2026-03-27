633009e97931152ec0da367e42c5833d9acecbcc
João Dias <joao.teixeira@grad.iprj.uerj.br>
2026-03-26 22:36:49 -0300

    ...

--- Exercicios_Iniciante/beecrowd1005.py
+++ Exercicios_Iniciante/beecrowd1005.py
@@ -1,4 +1,3 @@
 A = float(input())
 B = float(input())
 MEDIA = ((A * 3.5) + (B * 7.5)) / 2
-print(f"MEDIA={A:.5f}")
---
+++ Exercicios_Iniciante/beecrowd1008.py
@@ -0,0 +1,7 @@
+number = int(input())
+horas = int(input())
+hsalario = float(input())
+
+salario = horas * hsalario
+
+print(f"NUMBER = {number}\nSALARY = U$ {salario:.2f}")
